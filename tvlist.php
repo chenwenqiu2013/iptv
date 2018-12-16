b1948f12c488af75519fcf12800dd112875a6181
chenwenqiu2013 <chenwenqiu2013@gmail.com>
2018-12-16 20:48:27 +0800

    Add files via upload

---
+++ tvlist.php
@@ -0,0 +1,17 @@
+{"Auth": "true",
+"Announcement": " ",
+"Date": "",
+"IsTrial": "false",
+"LoginName": "666666",
+"Channel": [{"CategoryName":"IPTV","Password":"","Channel":[
+{"Id":1,"Num":"1","Name":"翡翠台(澳门版)","Url":"msc://CJMacau5"},
+{"Id":2,"Num":"2","Name":"翡翠台(澳洲版)","Url":"msc://CJAU5"},
+{"Id":3,"Num":"3","Name":"星河频道","Url":"msc://CXHMovie"},
+{"Id":4,"Num":"4","Name":"功夫台","Url":"msc://CKF5"},
+{"Id":5,"Num":"5","Name":"韩剧台","Url":"msc://CKDrama5"},
+{"Id":6,"Num":"6","Name":"新闻台","Url":"msc://CIN5"},
+{"Id":7,"Num":"7","Name":"财经台","Url":"msc://CFinance"},
+{"Id":8,"Num":"8","Name":"粤语片","Url":"msc://CCMovie"},
+{"Id":9,"Num":"9","Name":"无线卫星亚洲台","Url":"msc://CTVBSAsia"}
+]}
+]}
